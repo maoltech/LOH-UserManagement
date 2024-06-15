ec6422a7f8046fff6a2ad946c2acbf44db933fe6
mujeeb olagunju <hardeyshinnah.mo@gmail.com>
2024-06-15 13:13:29 +0100

    jwr

--- Core/Settings/JwtService.cs
+++ Core/Settings/JwtService.cs
@@ -1,44 +1,50 @@
-﻿
-
-using LOH_UserManagement.Entities;
-using Microsoft.Extensions.Options;
+﻿using System;
+using System.Collections.Generic;
 using System.Security.Claims;
 using System.Text;
-using Microsoft.IdentityModel.Tokens;
+using JWT;
+using JWT.Algorithms;
+using JWT.Serializers;
+using Microsoft.Extensions.Options;
+using LOH_UserManagement.Entities;
 
 namespace LOH_UserManagement.Core.Settings
 {
     public static class JwtService
-    //: IJwtService
     {
         public static string GenerateToken(LOHUser user, IOptions<JwtData> options)
         {
             var jWTData = options.Value;
-            var claims = new List<Claim>
-                {
-                    new Claim(ClaimTypes.NameIdentifier, user.Id),
-                    new Claim(ClaimTypes.Email, user.Email),
-                    new Claim(ClaimTypes.Name, user.Fullname),
-                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
-                };
 
-            
-            
-            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jWTData.SecretKey));
-
-
-            var tokenDescriptor = new SecurityTokenDescriptor
+            var claims = new Dictionary<string, object>
             {
-                Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.Now + jWTData.TokenLifeTime,
-                Audience = jWTData.Issuer,
-                Issuer = jWTData.Issuer,
-                SigningCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature)
+                { ClaimTypes.NameIdentifier, user.Id.ToString() },
+                { ClaimTypes.Email, user.Email },
+                { ClaimTypes.Name, user.UserName },
+                { "Fullname", user.Fullname },
+                { "IsAdmin", user.IsAdmin },
+                { "IsBuyer", user.IsBuyer },
+                { "IsSeller", user.IsSeller },
+                { "IsEmailVerified", user.IsEmailVerified }
             };
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var token = tokenHandler.CreateToken(tokenDescriptor);
 
-            return tokenHandler.WriteToken(token);
+            var payload = new Dictionary<string, object>
+            {
+                { "iss", jWTData.Issuer },
+                { "aud", jWTData.Audience },
+                { "exp", new DateTimeOffset(DateTime.UtcNow.AddMinutes(jWTData.ExpirationMinutes)).ToUnixTimeSeconds() },
+                { "iat", new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds() },
+                { "nbf", new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds() },
+                { "claims", claims }
+            };
+
+            IJwtAlgorithm algorithm = new HMACSHA256Algorithm(); // symmetric
+            IJsonSerializer serializer = new JsonNetSerializer();
+            IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+            IJwtEncoder encoder = new JwtEncoder(algorithm, serializer, urlEncoder);
+
+            var token = encoder.Encode(payload, jWTData.Secret);
+            return token;
         }
     }
 }
